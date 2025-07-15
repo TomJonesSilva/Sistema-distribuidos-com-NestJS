80bae65cce0bcd688801437e850f3652acfb3394
Tom-Jones <tom.jones.silva@gmail.com>
2025-07-15 16:24:05 -0300

    pequena alteração no module

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { UsuarioGatewayController } from './controllers/usuario-gateway.controller';
 import { UsuarioGatewayService } from './services/usuario-gateway.service';
+import { OpcaoController } from './controllers/opcao-gateway.controller';
+import { TicketController } from './controllers/ticket-gateway.controller';
+import { CardapioController } from './controllers/cardapio-gateway.controller';
+import { OpcaoService } from './services/opcao-gateway.service';
+import { TicketService } from './services/ticket-gateway.service';
+import { CardapioService } from './services/cardapio-gateway.service';
 
 @Module({
   imports: [],
-  controllers: [UsuarioGatewayController],
-  providers: [UsuarioGatewayService],
+  controllers: [
+    UsuarioGatewayController,
+    OpcaoController,
+    TicketController,
+    CardapioController,
+  ],
+  providers: [
+    UsuarioGatewayService,
+    OpcaoService,
+    TicketService,
+    CardapioService,
+  ],
 })
 export class AppModule {}
